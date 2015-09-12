306b01de84fe4b9acc43371061f5f4421b7f7d45
Sean Farrell <sean.farrell@rioki.org>
2015-09-12 17:36:44 +0200

    Adds basic functions.

--- src/functions.js
+++ src/functions.js
@@ -1,2 +1,135 @@
 
+rgm.add = function (a, b) {
+  if (a.length != b.length) {
+    throw Error('add: a and b must be the same dimension');
+  }
+  
+  var r = new Float32Array(a.length);
+  for (var i = 0; i < a.length; i++) {
+    r[i] = a[i] + b[i];
+  }
+  
+  return r;
+}
 
+rgm.sub = function (a, b) {
+  if (a.length != b.length) {
+    throw Error('sub: a and b must be the same dimension');
+  }
+  
+  var r = new Float32Array(a.length);
+  for (var i = 0; i < a.length; i++) {
+    r[i] = a[i] - b[i];
+  }
+  
+  return r;
+}
+
+rgm.neg = function (a) {
+  var r = new Float32Array(a.length);
+  for (var i = 0; i < a.length; i++) {
+    r[i] = -a[i];
+  }
+  
+  return r;
+}
+
+rgm.dot = function (a, b) {
+  var v = 0;
+  for (var i = 0; i < a.length; i++) {
+    v += a[i] * b[i];
+  }
+  return v;
+}
+
+rgm.cross = function (a, b) {
+  if (a.length != 3 ||
+      b.length != 3) {
+    throw Error('cross: a and b must be 3D');
+  }
+  
+  var r = new Float32Array(3);
+  
+  r[0] = (a[1] * b[2]) - (a[2] * b[1]);
+  r[1] = (a[2] * b[0]) - (a[0] * b[2]);
+  r[2] = (a[0] * b[1]) - (a[1] * b[0]);
+  
+  return r;
+}
+
+rgm.vmult = function (a, b) {
+  if (a.length != b.length) {
+    throw Error('vmult: a and b must be the same dimension');
+  }
+  
+  var r = new Float32Array(a.length);
+  for (var i = 0; i < a.length; i++) {
+    r[i] = a[i] * b[i];
+  }
+  return r;
+}
+
+rgm.svmult = function (v, s) {
+  var r = new Float32Array(v.length);
+  for (var i = 0; i < v.length; i++) {
+    r[i] = v[i] * s;
+  }
+  return r;
+}
+
+rgm.length = function (v) {  
+  return Math.sqrt(rgm.dot(v, v));
+}
+
+rgm.normalize = function (v) {
+  return rgm.svmult(v, 1.0 / rgm.length(v));
+}
+
+rgm.mmult = function (a, b) {
+  if (a.length != b.length) {
+    throw Error('mmult: a and b must be the same dimension');
+  }
+  
+  var n = Math.sqrt(a.length);
+  var r = new Float32Array(a.length);
+  
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      var v = 0;
+      for (var k = 0; k < n; k++) {
+        v = v + a[i*n+k] * b[k*n+j];
+      }
+      r[i*n+j] = v;
+    }
+  }
+  
+  return r;
+}
+
+rgm.mvmult = function (m, v) {
+	var n = v.length;
+	var r = new Float32Array(n);
+	
+	for (var i = 0; i < n; i++)
+	{
+		r[i] = 0;
+		for (var j = 0; j < n; j++)
+		{
+				r[i] += m[i*n+j] * v[j];
+		}
+	}
+	
+	return r;
+}
+
+rgm.qmult = function (a, b) {
+  var wa = a[3];
+  var va = rgm.vec3(a[0], a[1], a[2]);
+  var wb = b[3];
+  var vb = rgm.vec3(b[0], b[1], b[2]);
+  
+  var w = wa * wb - rgm.dot(va, vb);
+  var v = rgm.add(rgm.add(rgm.svmult(vb, wa), rgm.svmult(va, wb)), rgm.cross(va, vb));
+
+  return rgm.quat(v[0], v[1], va[2], w);
+}
---
+++ test/function-tests.js
@@ -0,0 +1,84 @@
+
+QUnit.module("general functions");
+QUnit.test("add", function (assert) {
+  var a = rgm.vec3(1, 2, 3);
+  var b = rgm.vec3(4, 5, 6);
+  var v = rgm.add(a, b);
+  assert.deepEqual(v, rgm.vec3(5, 7, 9));
+});
+
+QUnit.test("sub", function (assert) {
+  var a = rgm.vec3(8, 7, 6);
+  var b = rgm.vec3(3, 4, 5);
+  var v = rgm.sub(a, b);
+  assert.deepEqual(v, rgm.vec3(5, 3, 1));
+});
+
+QUnit.test("neg", function (assert) {
+  var a = rgm.vec3(8, 7, 6);
+  var v = rgm.neg(a);
+  assert.deepEqual(v, rgm.vec3(-8, -7, -6));
+});
+
+QUnit.test("dot", function (assert) {
+  var a = rgm.vec3(8, 7, 6);
+  var b = rgm.vec3(3, 4, 5);
+  var v = rgm.dot(a, b);
+  assert.deepEqual(v, 82.0);
+});
+
+QUnit.test("cross", function (assert) {
+  var a = rgm.vec3(1, 0, 0);
+  var b = rgm.vec3(0, 1, 0);
+  var v = rgm.cross(a, b);
+  assert.deepEqual(v, rgm.vec3(0, 0, 1));
+});
+
+QUnit.test("vmult", function (assert) {
+  var a = rgm.vec3(8, 7, 6);
+  var b = rgm.vec3(3, 4, 5);
+  var v = rgm.vmult(a, b);
+  assert.deepEqual(v, rgm.vec3(24, 28, 30));
+});
+
+QUnit.test("svmult", function (assert) {
+  var a = rgm.vec3(1, 2, 3);
+  var v = rgm.svmult(a, 0.5);
+  assert.deepEqual(v, rgm.vec3(0.5, 1.0, 1.5));
+});
+
+QUnit.test("length", function (assert) {
+  var a = rgm.vec3(1.0, 2.0, 3.0);
+  var v = rgm.length(a);
+  assert.equal(v, 3.7416573867739413);
+});
+
+QUnit.test("normalize", function (assert) {
+  var a = rgm.vec3(1.0, 2.0, 3.0);
+  var v = rgm.normalize(a);
+  var l = rgm.length(v);
+  assert.ok(Math.abs(l - 1.0) < 0.0001);
+});
+
+QUnit.test("mmult", function (assert) {
+  var a = rgm.mat3(3);
+  var b = rgm.mat3(2);
+  var v = rgm.mmult(a, b);
+  assert.deepEqual(v, rgm.mat3(6));
+});
+
+QUnit.test("mvmult", function (assert) {
+  var a = rgm.mat3(1, 0, 0,
+                   0, 2, 0,
+                   0, 0, 3);
+  var b = rgm.vec3(1, 2, 3);
+  var v = rgm.mvmult(a, b);
+  assert.deepEqual(v, rgm.vec3(1, 4, 9));
+});
+
+QUnit.test("qmult", function (assert) {
+  var a = rgm.quat(0, 0, 0, 1);
+  var b = rgm.quat(1, 2, 3, 0.5);
+  var v = rgm.qmult(a, b);
+  assert.deepEqual(v, rgm.quat(1, 2, 0, 0.5));
+});
