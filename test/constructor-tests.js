16fcbbf88c2427e02ea639cd91051d1d1ddc769e
Sean Farrell <sean.farrell@rioki.org>
2015-09-12 16:58:39 +0200

    Syntax cleanup.

--- test/constructor-tests.js
+++ test/constructor-tests.js
@@ -1,27 +1,27 @@
 
 QUnit.module("vec2");
-QUnit.test( "vec2 default is zero", function( assert ) {
+QUnit.test("vec2 default is zero", function (assert) {
   var v = rgm.vec2();
   assert.equal(2, v.length);
   assert.equal(0.0, v[0]);
   assert.equal(0.0, v[1]);
 });
 
-QUnit.test( "vec2 single value", function( assert ) {
+QUnit.test("vec2 single value", function (assert) {
   var v = rgm.vec2(1.0);
   assert.equal(2, v.length);
   assert.equal(1.0, v[0]);
   assert.equal(1.0, v[1]);
 });
 
-QUnit.test( "vec2 init with single values", function( assert ) {
+QUnit.test("vec2 init with single values", function (assert) {
   var v = rgm.vec2(1, 2);
   assert.equal(2, v.length);
   assert.equal(1.0, v[0]);
   assert.equal(2.0, v[1]);
 });
 
-QUnit.test( "vec2 init with array", function( assert ) {
+QUnit.test("vec2 init with array", function (assert) {
   var v = rgm.vec2([1, 2]);
   assert.equal(2, v.length);
   assert.equal(1.0, v[0]);
@@ -29,7 +29,7 @@ QUnit.test( "vec2 init with array", function( assert ) {
 });
 
 QUnit.module("vec3");
-QUnit.test( "vec3 default is zero", function( assert ) {
+QUnit.test("vec3 default is zero", function (assert) {
   var v = rgm.vec3();
   assert.equal(3, v.length);
   assert.equal(0.0, v[0]);
@@ -37,7 +37,7 @@ QUnit.test( "vec3 default is zero", function( assert ) {
   assert.equal(0.0, v[2]);
 });
 
-QUnit.test( "vec3 single value", function( assert ) {
+QUnit.test("vec3 single value", function (assert) {
   var v = rgm.vec3(1.0);
   assert.equal(3, v.length);
   assert.equal(1.0, v[0]);
@@ -45,7 +45,7 @@ QUnit.test( "vec3 single value", function( assert ) {
   assert.equal(1.0, v[2]);
 });
 
-QUnit.test( "vec3 init with single values", function( assert ) {
+QUnit.test("vec3 init with single values", function (assert) {
   var v = rgm.vec3(1, 2, 3);
   assert.equal(3, v.length);
   assert.equal(1.0, v[0]);
@@ -53,7 +53,7 @@ QUnit.test( "vec3 init with single values", function( assert ) {
   assert.equal(3.0, v[2]);
 });
 
-QUnit.test( "vec3 init with array", function( assert ) {
+QUnit.test("vec3 init with array", function (assert) {
   var v = rgm.vec3([1, 2, 3]);
   assert.equal(3, v.length);
   assert.equal(1.0, v[0]);
@@ -62,7 +62,7 @@ QUnit.test( "vec3 init with array", function( assert ) {
 });
 
 QUnit.module("vec4");
-QUnit.test( "vec4 default is zero", function( assert ) {
+QUnit.test("vec4 default is zero", function (assert) {
   var v = rgm.vec4();
   assert.equal(4, v.length);
   assert.equal(0.0, v[0]);
@@ -71,7 +71,7 @@ QUnit.test( "vec4 default is zero", function( assert ) {
   assert.equal(0.0, v[3]);
 });
 
-QUnit.test( "vec4 single value", function( assert ) {
+QUnit.test("vec4 single value", function (assert) {
   var v = rgm.vec4(1.0);
   assert.equal(4, v.length);
   assert.equal(1.0, v[0]);
@@ -80,7 +80,7 @@ QUnit.test( "vec4 single value", function( assert ) {
   assert.equal(1.0, v[3]);
 });
 
-QUnit.test( "vec4 init with single values", function( assert ) {
+QUnit.test("vec4 init with single values", function (assert) {
   var v = rgm.vec4(1, 2, 3, 4);
   assert.equal(4, v.length);
   assert.equal(1.0, v[0]);
@@ -89,7 +89,7 @@ QUnit.test( "vec4 init with single values", function( assert ) {
   assert.equal(4.0, v[3]);
 });
 
-QUnit.test( "vec4 init with array", function( assert ) {
+QUnit.test("vec4 init with array", function (assert) {
   var v = rgm.vec3([1, 2, 3, 4]);
   assert.equal(4, v.length);
   assert.equal(1.0, v[0]);
@@ -99,7 +99,7 @@ QUnit.test( "vec4 init with array", function( assert ) {
 });
 
 QUnit.module("mat3");
-QUnit.test( "mat3 default is identity", function( assert ) {
+QUnit.test("mat3 default is identity", function (assert) {
   var m = rgm.mat3();
   assert.equal(9, m.length);
   
@@ -110,7 +110,7 @@ QUnit.test( "mat3 default is identity", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat3 init single value", function( assert ) {
+QUnit.test("mat3 init single value", function (assert) {
   var m = rgm.mat3(2.0);
   assert.equal(9, m.length);
   
@@ -121,7 +121,7 @@ QUnit.test( "mat3 init single value", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat3 init array", function( assert ) {
+QUnit.test("mat3 init array", function (assert) {
   
   // NOTE: rwo -> column (openGL style)
   var m = rgm.mat3([1.0, 2.0, 3.0,
@@ -137,7 +137,7 @@ QUnit.test( "mat3 init array", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat3 init single values", function( assert ) {
+QUnit.test("mat3 init single values", function (assert) {
   
   // NOTE: rwo -> column (openGL style)
   var m = rgm.mat3(1.0, 2.0, 3.0,
@@ -154,7 +154,7 @@ QUnit.test( "mat3 init single values", function( assert ) {
 });
 
 QUnit.module("mat4");
-QUnit.test( "mat4 default is identity", function( assert ) {
+QUnit.test("mat4 default is identity", function (assert) {
   var m = rgm.mat4();
   assert.equal(16, m.length);
   
@@ -166,7 +166,7 @@ QUnit.test( "mat4 default is identity", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat4 init single value", function( assert ) {
+QUnit.test("mat4 init single value", function (assert) {
   var m = rgm.mat4(2.0);
   assert.equal(16, m.length);
   
@@ -178,7 +178,7 @@ QUnit.test( "mat4 init single value", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat4 init array", function( assert ) {
+QUnit.test("mat4 init array", function (assert) {
   
   // NOTE: rwo -> column (openGL style)
   var m = rgm.mat4([ 1.0,  2.0,  3.0,  4.0,
@@ -196,10 +196,10 @@ QUnit.test( "mat4 init array", function( assert ) {
   assert.deepEqual(r, m);
 });
 
-QUnit.test( "mat4 init single values", function( assert ) {
+QUnit.test("mat4 init single values", function (assert) {
   
   // NOTE: rwo -> column (openGL style)
-  var m = rgm.mat4( 1.0,  2.0,  3.0,  4.0,
+  var m = rgm.mat4(1.0,  2.0,  3.0,  4.0,
                     5.0,  6.0,  7.0,  8.0,
                     9.0, 10.0, 11.0, 12.0,
                    13.0, 14.0, 15.0, 16.0);
@@ -215,7 +215,7 @@ QUnit.test( "mat4 init single values", function( assert ) {
 });
 
 QUnit.module("quat");
-QUnit.test( "quat default", function( assert ) {
+QUnit.test("quat default", function (assert) {
   var v = rgm.quat();
   assert.equal(4, v.length);
   assert.equal(0.0, v[0]);
@@ -224,7 +224,7 @@ QUnit.test( "quat default", function( assert ) {
   assert.equal(1.0, v[3]);
 });
 
-QUnit.test( "quat init array", function( assert ) {
+QUnit.test("quat init array", function (assert) {
   var v = rgm.quat([1, 2, 3, 4]);
   assert.equal(4, v.length);
   assert.equal(1.0, v[0]);
@@ -233,7 +233,7 @@ QUnit.test( "quat init array", function( assert ) {
   assert.equal(4.0, v[3]);
 });
 
-QUnit.test( "quat init values", function( assert ) {
+QUnit.test("quat init values", function (assert) {
   var v = rgm.quat(1, 2, 3, 4);
   assert.equal(4, v.length);
   assert.equal(1.0, v[0]);
