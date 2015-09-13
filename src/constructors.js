5d1e16edea8044dc73b852924742d7fc2d162798
Sean Farrell <sean.farrell@rioki.org>
2015-09-13 11:37:54 +0200

    Adds conversion constructors.

--- src/constructors.js
+++ src/constructors.js
@@ -9,7 +9,15 @@ rgm.vec2 = function () {
         return new Float32Array([arguments[0], arguments[0]]);
       }
       else {
-        return new Float32Array(arguments[0]);
+        switch (arguments[0].length) {
+          case 2:
+            return new Float32Array(arguments[0]);        
+          case 3:
+          case 4:
+            return new Float32Array([arguments[0][0], arguments[0][1]]);        
+          default:
+            throw new Error('vec2: invalid array size');
+        }
       }
     case 2:
       return new Float32Array([arguments[0], arguments[1]]);
@@ -27,9 +35,18 @@ rgm.vec3 = function () {
       if (typeof arguments[0] === "number") {
         return new Float32Array([arguments[0], arguments[0], arguments[0]]);
       }
-      else {
-        return new Float32Array(arguments[0]);
-      }
+      else {        
+        switch (arguments[0].length) {
+          case 2:
+            return new Float32Array([arguments[0][0], arguments[0][1], 0.0]);        
+          case 3:
+            return new Float32Array(arguments[0]);        
+          case 4:
+            return new Float32Array([arguments[0][0], arguments[0][1], arguments[0][2]]);        
+          default:
+            throw new Error('vec3: invalid array size');
+        }       
+      }      
     case 3:
       return new Float32Array([arguments[0], arguments[1], arguments[2]]);
     default:
@@ -46,8 +63,17 @@ rgm.vec4 = function () {
       if (typeof arguments[0] === "number") {
         return new Float32Array([arguments[0], arguments[0], arguments[0], arguments[0]]);
       }
-      else {
-        return new Float32Array(arguments[0]);
+      else {        
+        switch (arguments[0].length) {
+          case 2:
+            return new Float32Array([arguments[0][0], arguments[0][1], 0.0, 0.0]);        
+          case 3:
+            return new Float32Array([arguments[0][0], arguments[0][1], arguments[0][2], 0.0]);        
+          case 4:
+            return new Float32Array(arguments[0]);        
+          default:
+            throw new Error('vec3: invalid array size');
+        }
       }
     case 4:
       return new Float32Array([arguments[0], arguments[1], arguments[2], arguments[3]]);
@@ -70,7 +96,16 @@ rgm.mat3 = function () {
                                           0.0,          0.0, arguments[0]]);
       }
       else {
-        return new Float32Array(arguments[0]);
+        switch (arguments[0].length) {
+          case 9:
+            return new Float32Array(arguments[0]);        
+          case 16:
+            return new Float32Array([arguments[0][0], arguments[0][1], arguments[0][ 2],
+                                     arguments[0][4], arguments[0][5], arguments[0][ 6],
+                                     arguments[0][8], arguments[0][9], arguments[0][10]]);
+          default:
+            throw new Error('mat3: invalid array size');
+        }        
       }
     case 9:
       return new Float32Array([arguments[0], arguments[1], arguments[2], 
@@ -97,7 +132,17 @@ rgm.mat4 = function () {
                                           0.0,          0.0,          0.0, arguments[0]]);
       }
       else {
-        return new Float32Array(arguments[0]);
+        switch (arguments[0].length) {
+          case 9:
+            return new Float32Array([arguments[0][0], arguments[0][1], arguments[0][2], 0.0,
+                                     arguments[0][3], arguments[0][4], arguments[0][5], 0.0,
+                                     arguments[0][6], arguments[0][7], arguments[0][8], 0.0,
+                                                 0.0,             0.0,             0.0, 1.0]);
+          case 16:
+            return new Float32Array(arguments[0]);
+          default:
+            throw new Error('mat3: invalid array size');
+        }        
       }
     case 16:
       return new Float32Array([arguments[ 0], arguments[ 1], arguments[ 2], arguments[ 3], 
--- test/constructor-tests.js
+++ test/constructor-tests.js
@@ -2,123 +2,177 @@
 QUnit.module("vec2");
 QUnit.test("vec2 default is zero", function (assert) {
   var v = rgm.vec2();
-  assert.equal(2, v.length);
-  assert.equal(0.0, v[0]);
-  assert.equal(0.0, v[1]);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 0.0);
+  assert.equal(v[1], 0.0);
 });
 
 QUnit.test("vec2 single value", function (assert) {
   var v = rgm.vec2(1.0);
-  assert.equal(2, v.length);
-  assert.equal(1.0, v[0]);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 1.0);
   assert.equal(1.0, v[1]);
 });
 
 QUnit.test("vec2 init with single values", function (assert) {
   var v = rgm.vec2(1, 2);
-  assert.equal(2, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
 });
 
 QUnit.test("vec2 init with array", function (assert) {
   var v = rgm.vec2([1, 2]);
-  assert.equal(2, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+});
+
+QUnit.test("vec2 from vec3", function (assert) {
+  var o = rgm.vec3(1, 2, 3);
+  var v = rgm.vec2(o);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+});
+
+QUnit.test("vec2 from vec4", function (assert) {
+  var o = rgm.vec4(1, 2, 3, 4);
+  var v = rgm.vec2(o);
+  assert.equal(v.length, 2);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
 });
 
 QUnit.module("vec3");
 QUnit.test("vec3 default is zero", function (assert) {
   var v = rgm.vec3();
-  assert.equal(3, v.length);
-  assert.equal(0.0, v[0]);
-  assert.equal(0.0, v[1]);
-  assert.equal(0.0, v[2]);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 0.0);
+  assert.equal(v[1], 0.0);
+  assert.equal(v[2], 0.0);
 });
 
 QUnit.test("vec3 single value", function (assert) {
   var v = rgm.vec3(1.0);
-  assert.equal(3, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(1.0, v[1]);
-  assert.equal(1.0, v[2]);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 1.0);
+  assert.equal(v[2], 1.0);
 });
 
 QUnit.test("vec3 init with single values", function (assert) {
   var v = rgm.vec3(1, 2, 3);
-  assert.equal(3, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
 });
 
 QUnit.test("vec3 init with array", function (assert) {
   var v = rgm.vec3([1, 2, 3]);
-  assert.equal(3, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
+});
+
+QUnit.test("vec3 from vec2", function (assert) {
+  var o = rgm.vec2(1, 2);
+  var v = rgm.vec3(o);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 0.0);
+});
+
+QUnit.test("vec3 from vec4", function (assert) {
+  var o = rgm.vec4(1, 2, 3, 4);
+  var v = rgm.vec3(o);
+  assert.equal(v.length, 3);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
 });
 
 QUnit.module("vec4");
 QUnit.test("vec4 default is zero", function (assert) {
   var v = rgm.vec4();
-  assert.equal(4, v.length);
-  assert.equal(0.0, v[0]);
-  assert.equal(0.0, v[1]);
-  assert.equal(0.0, v[2]);
-  assert.equal(0.0, v[3]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 0.0);
+  assert.equal(v[1], 0.0);
+  assert.equal(v[2], 0.0);
+  assert.equal(v[3], 0.0);
 });
 
 QUnit.test("vec4 single value", function (assert) {
   var v = rgm.vec4(1.0);
-  assert.equal(4, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(1.0, v[1]);
-  assert.equal(1.0, v[2]);
-  assert.equal(1.0, v[3]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 1.0);
+  assert.equal(v[2], 1.0);
+  assert.equal(v[3], 1.0);
 });
 
 QUnit.test("vec4 init with single values", function (assert) {
   var v = rgm.vec4(1, 2, 3, 4);
-  assert.equal(4, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
-  assert.equal(4.0, v[3]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
+  assert.equal(v[3], 4.0);
 });
 
 QUnit.test("vec4 init with array", function (assert) {
-  var v = rgm.vec3([1, 2, 3, 4]);
-  assert.equal(4, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
-  assert.equal(4.0, v[3]);
+  var v = rgm.vec4([1, 2, 3, 4]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
+  assert.equal(v[3], 4.0);
+});
+
+QUnit.test("vec3 from vec2", function (assert) {
+  var o = rgm.vec2(1, 2);
+  var v = rgm.vec4(o);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 0.0);
+  assert.equal(v[3], 0.0);
+});
+
+QUnit.test("vec4 from vec3", function (assert) {
+  var o = rgm.vec3(1, 2, 3);
+  var v = rgm.vec4(o);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
+  assert.equal(v[3], 0.0);
 });
 
 QUnit.module("mat3");
 QUnit.test("mat3 default is identity", function (assert) {
   var m = rgm.mat3();
-  assert.equal(9, m.length);
+  assert.equal(m.length, 9);
   
   var r = new Float32Array([1.0, 0.0, 0.0,
                             0.0, 1.0, 0.0,
                             0.0, 0.0, 1.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat3 init single value", function (assert) {
   var m = rgm.mat3(2.0);
-  assert.equal(9, m.length);
+  assert.equal(m.length, 9);
   
   var r = new Float32Array([2.0, 0.0, 0.0,
                             0.0, 2.0, 0.0,
                             0.0, 0.0, 2.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat3 init array", function (assert) {
@@ -128,13 +182,13 @@ QUnit.test("mat3 init array", function (assert) {
                     4.0, 5.0, 6.0,
                     7.0, 8.0, 9.0]);
   
-  assert.equal(9, m.length);
+  assert.equal(m.length, 9);
   
   var r = new Float32Array([1.0, 2.0, 3.0,
                             4.0, 5.0, 6.0,
                             7.0, 8.0, 9.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat3 init single values", function (assert) {
@@ -144,38 +198,54 @@ QUnit.test("mat3 init single values", function (assert) {
                    4.0, 5.0, 6.0,
                    7.0, 8.0, 9.0);
   
-  assert.equal(9, m.length);
+  assert.equal(m.length, 9);
   
   var r = new Float32Array([1.0, 2.0, 3.0,
                             4.0, 5.0, 6.0,
                             7.0, 8.0, 9.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
+});
+
+QUnit.test("mat3 from mat4", function (assert) {
+  
+  var o = rgm.mat4( 1.0,  2.0,  3.0,  4.0,
+                    5.0,  6.0,  7.0,  8.0,
+                    9.0, 10.0, 11.0, 12.0,
+                   13.0, 14.0, 15.0, 16.0);
+  
+  var m = rgm.mat3(o);
+  
+  assert.equal(m.length, 9);
+  var r = rgm.mat3(1.0,  2.0,  3.0,
+                   5.0,  6.0,  7.0,
+                   9.0, 10.0, 11.0);
+  assert.deepEqual(m, r);
 });
 
 QUnit.module("mat4");
 QUnit.test("mat4 default is identity", function (assert) {
   var m = rgm.mat4();
-  assert.equal(16, m.length);
+  assert.equal(m.length, 16);
   
   var r = new Float32Array([1.0, 0.0, 0.0, 0.0,
                             0.0, 1.0, 0.0, 0.0,
                             0.0, 0.0, 1.0, 0.0,
                             0.0, 0.0, 0.0, 1.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat4 init single value", function (assert) {
   var m = rgm.mat4(2.0);
-  assert.equal(16, m.length);
+  assert.equal(m.length, 16);
   
   var r = new Float32Array([2.0, 0.0, 0.0, 0.0,
                             0.0, 2.0, 0.0, 0.0, 
                             0.0, 0.0, 2.0, 0.0,
                             0.0, 0.0, 0.0, 2.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat4 init array", function (assert) {
@@ -186,14 +256,14 @@ QUnit.test("mat4 init array", function (assert) {
                      9.0, 10.0, 11.0, 12.0,
                     13.0, 14.0, 15.0, 16.0]);
   
-  assert.equal(16, m.length);
+  assert.equal(m.length, 16);
   
   var r = new Float32Array([ 1.0,  2.0,  3.0,  4.0,
                              5.0,  6.0,  7.0,  8.0,
                              9.0, 10.0, 11.0, 12.0,
                             13.0, 14.0, 15.0, 16.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
 });
 
 QUnit.test("mat4 init single values", function (assert) {
@@ -204,40 +274,56 @@ QUnit.test("mat4 init single values", function (assert) {
                     9.0, 10.0, 11.0, 12.0,
                    13.0, 14.0, 15.0, 16.0);
   
-  assert.equal(16, m.length);
+  assert.equal(m.length, 16);
   
   var r = new Float32Array([ 1.0,  2.0,  3.0,  4.0,
                              5.0,  6.0,  7.0,  8.0,
                              9.0, 10.0, 11.0, 12.0,
                             13.0, 14.0, 15.0, 16.0]);
   
-  assert.deepEqual(r, m);
+  assert.deepEqual(m, r);
+});
+
+QUnit.test("mat4 from mat3", function (assert) {
+  
+  var o = rgm.mat3( 1.0, 2.0, 3.0,  
+                    4.0, 5.0, 6.0,  
+                    7.0, 8.0, 9.0);
+  
+  var m = rgm.mat4(o);
+  
+  assert.equal(m.length, 16);
+  var r = rgm.mat4(1.0, 2.0, 3.0, 0.0,
+                   4.0, 5.0, 6.0, 0.0,
+                   7.0, 8.0, 9.0, 0.0,
+                   0.0, 0.0, 0.0, 1.0);
+  assert.deepEqual(m, r);
 });
 
 QUnit.module("quat");
 QUnit.test("quat default", function (assert) {
   var v = rgm.quat();
-  assert.equal(4, v.length);
-  assert.equal(0.0, v[0]);
-  assert.equal(0.0, v[1]);
-  assert.equal(0.0, v[2]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 0.0);
+  assert.equal(v[1], 0.0);
+  assert.equal(v[2], 0.0);
   assert.equal(1.0, v[3]);
 });
 
 QUnit.test("quat init array", function (assert) {
   var v = rgm.quat([1, 2, 3, 4]);
-  assert.equal(4, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
   assert.equal(4.0, v[3]);
 });
 
 QUnit.test("quat init values", function (assert) {
   var v = rgm.quat(1, 2, 3, 4);
-  assert.equal(4, v.length);
-  assert.equal(1.0, v[0]);
-  assert.equal(2.0, v[1]);
-  assert.equal(3.0, v[2]);
+  assert.equal(v.length, 4);
+  assert.equal(v[0], 1.0);
+  assert.equal(v[1], 2.0);
+  assert.equal(v[2], 3.0);
   assert.equal(4.0, v[3]);
 });
